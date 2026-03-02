501b8002f01d40edea5cdbf1ddf027887745afb8
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-03-02 16:34:55 -0300

    lecture 9 - exercise

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>
+                Aqui vai ter informações sobre o site Indie Gamer, um lugar onde você pode encontrar os melhores jogos indie do mercado!
+            </p>
+        </>
+    );
+}
---
+++ app/reviews/hollow-knight/page.tsx
@@ -0,0 +1,10 @@
+export default function HollowKnightPage() {
+    return (
+        <>
+            <h1>Hollow Knight</h1>
+            <p>
+                Aqui vai ter o review do Hollow Knight, um jogo de metroidvania que é simplesmente incrível! Ele tem uma jogabilidade viciante, gráficos charmosos e uma trilha sonora maravilhosa. Se você gosta de jogos de metroidvania, com certeza vai adorar o Hollow Knight!
+            </p>
+        </>
+    );
+}

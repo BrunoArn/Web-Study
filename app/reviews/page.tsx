f10943da579e4a27d97871d8918c58ce00638079
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-03-02 15:10:56 -0300

    lecture 9 - Page Routes

---
+++ app/reviews/page.tsx
@@ -0,0 +1,10 @@
+export default function ReviewsPage() {
+    return (
+        <>
+            <h1>Reviews</h1>
+            <p>
+                Tem review pra caramba, mano! Tem review de tudo quanto é jogo indie, desde os mais famosos até os mais obscuros. Se você quer saber se um jogo é bom ou não, é só dar uma olhada aqui!
+            </p>
+        </>
+    );
+}
---
+++ app/reviews/stardew-valley/page.tsx
@@ -0,0 +1,10 @@
+export default function StardewValleyPage() {
+    return (
+        <>
+            <h1>Stardew-Valley</h1>
+            <p>
+                Aqui vai ter o review do Stardew Valley, um jogo de simulação de fazenda que é simplesmente incrível! Ele tem uma jogabilidade viciante, gráficos charmosos e uma trilha sonora maravilhosa. Se você gosta de jogos de simulação, com certeza vai adorar o Stardew Valley!
+            </p>
+        </>
+    );
+}

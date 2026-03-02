2cab61b4e87f053404137d6e2d86f14d6a4b806b
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-03-02 12:21:00 -0300

    lecture 6 done

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,15 @@ export default function rootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-
+                <header>
+                    [header]
+                </header>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    [footer]
+                </footer>
             </body>
         </html>
     );

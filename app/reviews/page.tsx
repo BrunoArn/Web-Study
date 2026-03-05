cdc461b37b0b4ca8075e1c3486d131edf35424ed
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-03-05 19:07:35 -0300

    lecture 26 - add images

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+
 export default function ReviewsPage() {
     return (
         <>
@@ -7,15 +8,27 @@ export default function ReviewsPage() {
             <p>
                 Tem review pra caramba, mano! Tem review de tudo quanto é jogo indie, desde os mais famosos até os mais obscuros. Se você quer saber se um jogo é bom ou não, é só dar uma olhada aqui!
             </p>
-            <ul>
-                <li>
+            <ul className="flex flex-col gap-3">
+                <li className="bg-white border w-80 rounded shadow hover:shadow-xl">
                     <Link href="/reviews/hollow-knight">
-                        hollow knight
+                        <img src="/images/hollow-knight.jpg"
+                            alt=""
+                            width={320} height={180} className="mb-2 rounded-t"
+                        />
+                        <h2 className="py-1 text-center">
+                            hollow knight
+                        </h2>
                     </Link>
                 </li>
-                <li>
+                 <li className="bg-white border w-80 rounded shadow hover:shadow-xl">
                     <Link href="/reviews/stardew-valley">
-                        Stardew Valley
+                        <img src="/images/stardew-valley.jpg"
+                            alt=""
+                            width={320} height={180} className="mb-2 rounded-t"
+                        />
+                        <h2 className="py-1 text-center">
+                            stardew valley
+                        </h2>
                     </Link>
                 </li>
             </ul>
--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -4,6 +4,10 @@ export default function StardewValleyPage() {
     return (
         <>
             <Heading>Stardew Valley</Heading>
+            <img src="/images/stardew-valley.jpg"
+                alt=""
+                width={640} height={360} className="mb-2 rounded"
+            />
             <p>
                 Aqui vai ter o review do Stardew Valley, um jogo de simulação de fazenda que é simplesmente incrível! Ele tem uma jogabilidade viciante, gráficos charmosos e uma trilha sonora maravilhosa. Se você gosta de jogos de simulação, com certeza vai adorar o Stardew Valley!
             </p>

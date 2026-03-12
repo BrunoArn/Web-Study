e4beadea6c62ffa6404f376acc7ce503b1958992
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-03-12 15:34:52 -0300

    lecture 35 - moludarizing and refactor

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,22 +1,17 @@
-import { readFile } from "node:fs/promises";
-import matter from "gray-matter";
-import { marked } from "marked";
 import Heading from "@/components/Heading";
+import { getReviewData } from "@/lib/reviews";
 
 export default async function StardewValleyPage() {
-    const text = await readFile("./content/reviews/stardew-valley.md", "utf-8");
-    const { content, data: { title, date, image } } = matter(text);
-    const html = marked(content);
-
+    const reviewData = await getReviewData("stardew-valley");
     return (
         <>
-            <Heading>{title}</Heading>
-            <p className="italic pb-2">{date} </p>
-            <img src={image}
+            <Heading>{reviewData.title}</Heading>
+            <p className="italic pb-2">{reviewData.date} </p>
+            <img src={reviewData.image}
                 alt=""
                 width={640} height={360} className="mb-2 rounded"
             />
-            <article dangerouslySetInnerHTML={{ __html: html }}
+            <article dangerouslySetInnerHTML={{ __html: reviewData.body }}
                 className="max-w-screen-sm prose prose-slate"
             />
         </>
---
+++ lib/reviews.ts
@@ -0,0 +1,10 @@
+import { readFile } from "node:fs/promises";
+import matter from "gray-matter";
+import { marked } from "marked";
+
+export async function getReviewData(slug: string) {
+    const text = await readFile(`./content/reviews/${slug}.md`, "utf-8");
+    const { content, data: { title, date, image } } = matter(text);
+    const body = marked(content);
+    return { title, date, image, body };
+}

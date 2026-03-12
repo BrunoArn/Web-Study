b8bb53064b0cdc2b8dffb10faf0650ec2a871f3c
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-03-12 14:46:20 -0300

    lecture 32 - rendering markdown and read file

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,6 +1,12 @@
-import Heading from "../../../components/Heading";
+import { readFile } from "node:fs/promises";
+import { marked } from "marked";
+import Heading from "@/components/Heading";
 
-export default function StardewValleyPage() {
+export default async function StardewValleyPage() {
+    const text = await readFile("./content/reviews/stardew-valley.md", "utf-8");
+    const html = marked(text);
+
+
     return (
         <>
             <Heading>Stardew Valley</Heading>
@@ -8,9 +14,7 @@ export default function StardewValleyPage() {
                 alt=""
                 width={640} height={360} className="mb-2 rounded"
             />
-            <p>
-                Aqui vai ter o review do Stardew Valley, um jogo de simulação de fazenda que é simplesmente incrível! Ele tem uma jogabilidade viciante, gráficos charmosos e uma trilha sonora maravilhosa. Se você gosta de jogos de simulação, com certeza vai adorar o Stardew Valley!
-            </p>
+            <article dangerouslySetInnerHTML={{__html: html}} />
         </>
     );
 }
---
+++ content/reviews/stardew-valley.md
@@ -0,0 +1,11 @@
+# Heading
+## Sub Heading 
+
+Rich text with __bold__ and _italic_ 
+
+paragraphs
+
+List:
+* one
+* two
+* tree

6b03804060378c132b3b74d56c755abcfe082100
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-05-07 18:12:26 -0300

    lecture 66 - refactioring fetch

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -11,58 +11,57 @@ export async function getFeaturedReview() {
 }
 
 export async function getReviewData(slug) {
-    // const text = await readFile(`./content/reviews/${slug}.md`, "utf-8");
-    // const { content, data: { title, date, image } } = matter(text);
-    // const body = marked(content);
-    // return { slug, title, date, image, body };
-
-    const url = `${CMS_URL}/api/reviews?`
-        + qs.stringify(
-            {
-                filters: { slug: { $eq: slug } },
-                fields: ['slug', 'Title', 'subtitle', 'publishedAt', 'Body'],
-                populate: { image: { fields: ['url'] } },
-                pagination: { pageSize: 1, withCount: false },
-            },
-            { encodeValuesOnly: true }
-        );
-
-    console.log('getReviewsList:', url);
-    const response = await fetch(url);
-    const { data } = await response.json();
+    const { data } = await FetchReviewsList(
+        {
+            filters: { slug: { $eq: slug } },
+            fields: ['slug', 'Title', 'subtitle', 'publishedAt', 'Body'],
+            populate: { image: { fields: ['url'] } },
+            pagination: { pageSize: 1, withCount: false },
+        });
 
     const review = data[0];
     return {
-        slug: review.slug,
-        title: review.Title,
-        date: review.publishedAt.slice(0, 'yyyy-mm-dd'.length),
-        image: CMS_URL + review.image?.url,
+        ...ToReview(review),
         body: marked(review.Body),
     };
 }
 
 export async function getReviewsList() {
-    const url = `${CMS_URL}/api/reviews?`
-        + qs.stringify({
+    const { data } = await FetchReviewsList(
+        {
             fields: ['slug', 'Title', 'subtitle', 'publishedAt'],
             populate: { image: { fields: ['url'] } },
             sort: ['publishedAt:desc'],
             pagination: { pageSize: 6 }
-        }, { encodeValuesOnly: true });
-    console.log('getReviewsList:', url);
+        });
 
-    const response = await fetch(url);
-    const { data } = await response.json();
-    return data.map((attributes) => ({
-        slug: attributes.slug,
-        title: attributes.Title,
-        date: attributes.publishedAt.slice(0, 'yyyy-mm-dd'.length),
-        image: CMS_URL + (attributes.image?.url)
-    }));
+    return data.map(ToReview);
 }
 
 export async function getSlugs() {
     const files = await readdir("./content/reviews");
     return files.filter((file) => file.endsWith(".md"))
         .map((file) => file.replace(".md", ""));
+}
+
+async function FetchReviewsList(parameters) {
+    const url = `${CMS_URL}/api/reviews?`
+        + qs.stringify(parameters, { encodeValuesOnly: true });
+
+    console.log('FetchReviewsList:', url);
+
+    const response = await fetch(url);
+    if(!response.ok) {
+        throw new Error(`Failed to fetch reviews list: ${response.status} for ${url}`);
+    }
+    return await response.json();
+}
+
+function ToReview(item) {
+    return {
+        slug: item.slug,
+        title: item.Title,
+        date: item.publishedAt.slice(0, 'yyyy-mm-dd'.length),
+        image: CMS_URL + (item.image?.url)
+    }
 }

ba9c3d075638c70d33b55db5d6ad65ab1031b028
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-05-07 16:48:41 -0300

    Strapi API parameters - lecture 61

--- app/scripts/strapi-request.mjs
+++ app/scripts/strapi-request.mjs
@@ -1,7 +1,16 @@
 import { writeFileSync } from "node:fs"
+import { title } from "node:process";
+import qs from "qs";
 
 const url = 'http://localhost:1337/api/reviews'
-+ '?populate=*';
+    + '?' + qs.stringify({
+    fields: ['slug', 'Title', 'subtitle', 'publishedAt'],
+    populate: { image: { fields: ['url'] } },
+    pagination: { pageSize: 6 }
+    }, { encodeValuesOnly: true });
+
+console.log('url:', url);
+
 const response = await fetch(url);
 const body = await response.json();
 

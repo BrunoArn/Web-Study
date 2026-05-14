96bf291fd933bc642d74ec34f0acd819c1a36cf6
Bruno Barreiro Bastos Arnaud Fonseca <bruno.arnaud.br@gmail.com>
2026-05-14 17:18:49 -0300

    lecture 100-101 - .env.local variables for url and imageUrl

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
-
 module.exports = {
   images: {
     dangerouslyAllowLocalIP: true,
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**",
-      }
+      toRemotePattern(process.env.CMS_IMAGE_PATTERN)
     ]
   }
 };
+
+function toRemotePattern(urlStrin) {
+  const url = new URL(urlStrin)
+  return (
+    {
+      protocol: url.protocol.slice(0, -1),
+      hostname: url.hostname,
+      port: url.port,
+      pathname: url.pathname,
+    }
+  );
+}

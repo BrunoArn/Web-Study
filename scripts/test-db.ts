857239a66dc46bdb26f943e26dfa70899cf7d643
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-05-15 19:23:11 -0300

    lecture 110 - Prisma Querys

---
+++ scripts/test-db.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL ?? "file:./dev.db",
+});
+
+const db = new PrismaClient({ adapter,
+     log: [{ emit: "stdout", level: "query" }] });
+
+const comment = await db.comment.create({
+    data: {
+        slug: "diablo-iv",
+        user: "Wayne",
+        message: "Ok game!",
+    },
+});
+
+console.log("Created comment:", comment);
+const comments = await db.comment.findMany({
+    where: { slug: "diablo-iv" }
+});
+
+console.log("Found comments:", comments);
+await db.$disconnect();
